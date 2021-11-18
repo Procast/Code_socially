1aa71d24f7f6a9fa39c0c04800c1a2ed27017ff3
Prashant kumar <prkunar28@gmail.com>
2021-11-18 20:58:23 +0530

    add posts to the home views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,22 @@
+const Post=require("../models/post");
+
+
 module.exports.home=function(req,res){
 
     console.log(req.cookies);
-    return res.render('home',{
-        title:'Home'
+   
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        if (err){
+            console.log("Error in finding the posts");
+            return;
+        }
+
+        return res.render('home',{
+            title:'Home',
+            posts:posts
+        });
     });
+
+    
 };
